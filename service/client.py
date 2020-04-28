2775d6c9f9681a317b0d0ed9a97378f7bbbe66d5
Younmin Bae <ybae35@gatech.edu>
2020-04-28 05:29:18 +0000

    added gitignore, command-line arg in client

--- service/client.py
+++ service/client.py
@@ -1,6 +1,7 @@
 import numpy as np
 import grpc
 import pickle
+import sys
 
 import matrix_op_pb2
 import matrix_op_pb2_grpc
@@ -10,7 +11,12 @@ def matmult(stub, a, b):
     message = matrix_op_pb2.OpRequest(a=pickle.dumps(a), b=pickle.dumps(b))
     return pickle.loads(stub.MatMult(message).res)
 
-with grpc.insecure_channel('localhost:50051') as channel:
+if (len(sys.argv) == 1):
+    server_addr = 'localhost:50051'
+else:
+    server_addr = sys.argv[1]
+
+with grpc.insecure_channel(server_addr) as channel:
     stub = matrix_op_pb2_grpc.MatrixOpStub(channel)
     a = np.random.rand(128, 128).astype(dtype=np.float32)
     b = np.random.rand(128, 128).astype(dtype=np.float32)
