b4247e43cc9765138676ffb3d202127abab4540a
Aditya Jituri <adi.jituri@gmail.com>
2020-11-03 16:23:27 -0500

    this may work

--- service/client.py
+++ service/client.py
@@ -3,20 +3,20 @@ import grpc
 import pickle
 import sys
 import time
+import asyncio
 
 import matrix_op_pb2
 import matrix_op_pb2_grpc
 
 # handles np array pickling & unpickling
-def matmult(stub, a, b):
-    before = time.time()
-
+async def startmatmult(stub, a, b):
+    #before = time.time()
     message = matrix_op_pb2.OpRequest(a=pickle.dumps(a), b=pickle.dumps(b))
-    reply = stub.MatMult(message)
+    reply = await stub.MatMult(message)
     res =  pickle.loads(reply.res)
-
-    after = time.time()
-    return (res, round(after - before, 2))
+    print(f'pynq: {res} s')
+    #after = time.time()
+    #return (res, round(after - before, 2))
 
 
 if (len(sys.argv) == 1):
@@ -26,12 +26,14 @@ else:
 
 with grpc.insecure_channel(server_addr) as channel:
     stub = matrix_op_pb2_grpc.MatrixOpStub(channel)
-
-    a = np.random.rand(128, 128).astype(dtype=np.float32)
-    b = np.random.rand(128, 128).astype(dtype=np.float32)
-
-    res1 = matmult(stub, a, b)
-    print(f'pynq: {res1[1]} s')
+    before = time.time()
+    for _ in range(800):
+        a = np.random.rand(128, 128).astype(dtype=np.float32)
+        b = np.random.rand(128, 128).astype(dtype=np.float32)
+        asyncio.run(matmult(stub, a, b))
+    after = time.time()
+    lat1 = round((time.time() - before) * 1000000, 2)
+    print(f'800: {lat1} microsec')
 
     before = time.time()
     res2 = np.matmul(a, b)
